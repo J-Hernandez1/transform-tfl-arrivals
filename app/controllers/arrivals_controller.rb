68f18f1b873de1af732279d90ae1f801dda9682e
J-Hernandez1 <jakehernandez397@gmail.com>
2024-08-01 15:36:06 +0100

    add platforms and sort by time

--- app/controllers/arrivals_controller.rb
+++ app/controllers/arrivals_controller.rb
@@ -1,5 +1,7 @@
 class ArrivalsController < ApplicationController
   def index
-    @arrivals = ArrivalsService.new.arrivals.parsed_response
+    arrivals = ArrivalsService.new.arrivals.parsed_response
+
+    @arrivals = arrivals.sort_by { |arrival| arrival['expectedArrival'].to_datetime }
   end
 end

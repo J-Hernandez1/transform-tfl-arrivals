9c728a4e1b82b3cb4cfb1388a8999ae916d0cf26
J-Hernandez1 <jakehernandez397@gmail.com>
2024-08-01 16:17:14 +0100

    fix linting errors

--- app/services/arrivals_service.rb
+++ app/services/arrivals_service.rb
@@ -1,27 +1,27 @@
 # Service for arrival controler logic
 class ArrivalsService
-    require 'httparty'
-  
-    BASE_URL = 'https://api.tfl.gov.uk'.freeze
-  
-    def initialize(station_id)
-      @station_id = station_id
-    end
-  
-    def arrivals
-      response = HTTParty.get("#{BASE_URL}/StopPoint/#{@station_id}/Arrivals")
-      response.parsed_response
-    end
-  
-    def fetch_arrivals_data
-      arrivals_data = arrivals
-      sort_by_arrival_time(arrivals_data)
-    end
-  
-    private
-  
-    def sort_by_arrival_time(arrivals_data)
-      arrivals_data.map { |data| Arrival.new(data) }
-                   .sort_by { |arrival| arrival.expected_arrival.to_datetime }
-    end
+  require 'httparty'
+
+  BASE_URL = 'https://api.tfl.gov.uk'.freeze
+
+  def initialize(station_id)
+    @station_id = station_id
+  end
+
+  def arrivals
+    response = HTTParty.get("#{BASE_URL}/StopPoint/#{@station_id}/Arrivals")
+    response.parsed_response   
+  end
+
+  def fetch_arrivals_data
+    arrivals_data = arrivals
+    sort_by_arrival_time(arrivals_data)
+  end
+
+  private
+
+  def sort_by_arrival_time(arrivals_data)
+    arrivals_data.map { |data| Arrival.new(data) }
+                 .sort_by { |arrival| arrival.expected_arrival.to_datetime }
+  end
 end
